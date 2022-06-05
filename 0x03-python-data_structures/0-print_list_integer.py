c50d11c3922abb245056cc87ab48392c1756d867
Nixon Okoth <rgnixonroy@gmail.com>
2022-06-05 22:14:10 +0300

    task 0 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-
-    if (my_list):
-        for val in my_list:
-            print("{:d}".format(val))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
